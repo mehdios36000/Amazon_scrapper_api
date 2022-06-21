a916a2effbe1b391849f90b3e32ce2d4264ccb2f
mehdialamiidrissi <M.alamiidrissi@aui.ma>
2022-06-21 21:31:54 +0100

    solved redundant code use

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,12 +1,13 @@
 'use strict';
 const puppeteer = require('puppeteer');
+const url_computer="https://www.amazon.com/s?i=specialty-aps&bbn=16225009011&rh=n%3A%2116225009011%2Cn%3A281407&ref=nav_em__nav_desktop_sa_intl_accessories_and_supplies_0_2_5_2"
+const url_audio_video="https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A16225007011%2Cn%3A172456%2Cn%3A11548951011&dc&fs=true&ds=v1%3AhGungeZ2bDQ8vvZMlNdIg%2BnFSMrq8Y1rwfOcb9WUUb8&qid=1655840187&rnid=172456&ref=sr_nr_n_1"
 
 
-
-const getAllProductsComputer = async (req, res,next) => {
+const ScrapWebProducts = async (url) => {
     const Browser = await puppeteer.launch({ headless: true });
     const Page = await Browser.newPage();
-    await Page.goto('https://www.amazon.com/s?i=specialty-aps&bbn=16225009011&rh=n%3A%2116225009011%2Cn%3A281407&ref=nav_em__nav_desktop_sa_intl_accessories_and_supplies_0_2_5_2', { waitUntil: 'networkidle2' });
+    await Page.goto(url, { waitUntil: 'networkidle2' });
     const products = await Page.evaluate(() => {
         const products = Array.from(document.querySelectorAll('.sg-col-4-of-12.s-result-item.s-asin.sg-col-4-of-16.sg-col.s-widget-spacing-small.sg-col-4-of-20'));
         return products.map(product => {
@@ -29,35 +30,20 @@ const getAllProductsComputer = async (req, res,next) => {
     }
     );
     await Browser.close();
+    return products;
+
+}
+
+
+
+const getAllProductsComputer = async (req, res,next) => {
+    const products = await ScrapWebProducts(url_computer);
     res.status(200).json(products);
 
 }
 
 const getAllProductsVideoAndAudio = async (req, res,next) => {
- const Browser = await puppeteer.launch({ headless: true });
-    const Page = await Browser.newPage();
-    await Page.goto('https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A16225007011%2Cn%3A172456%2Cn%3A11548951011&dc&fs=true&ds=v1%3AhGungeZ2bDQ8vvZMlNdIg%2BnFSMrq8Y1rwfOcb9WUUb8&qid=1655840187&rnid=172456&ref=sr_nr_n_1', { waitUntil: 'networkidle2' });
-    const products = await Page.evaluate(() => {
-        const products = Array.from(document.querySelectorAll('.sg-col-4-of-12.s-result-item.s-asin.sg-col-4-of-16.sg-col.s-widget-spacing-small.sg-col-4-of-20'));
-        return products.map(product => {
-            const name = product.querySelector('.a-size-base-plus.a-color-base.a-text-normal').innerText;
-            let price;
-            let BestSeller= true;
-            try{
-                price= product.querySelector('.a-offscreen').innerText;
-            }
-            catch(err){
-                price="not available product";
-            }
-            if(product.querySelector(".a-badge-label")===null){
-                BestSeller=false;
-            }
-            return { name,price,BestSeller};
-        });
-
-    }
-    );
-    await Browser.close();
+    const products = await ScrapWebProducts(url_audio_video);
     res.status(200).json(products);
 }
 
